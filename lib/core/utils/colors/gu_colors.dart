472e20dce21b07ead4b7bb6d9fb61849c1d0d2d4
Andre Costa <andre_costa2012@hotmail.com.br>
2023-11-22 09:53:03 -0300

    feat: colors added

---
+++ lib/core/utils/colors/gu_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GUColors {
+  const GUColors._();
+
+  static const lightGray = Color(0xFFD9D9D9);
+
+  static const black = Color(0xFF000000);
+
+  static const white = Color(0xFFFFFFFF);
+
+  static const red = Color(0xFFFF4D4F);
+
+  static const green = Colors.green;
+
+  static const transparent = Colors.transparent;
+
+  static const primaryColor = Color(0xFF46E297);
+
+  static const secondaryColor = Color(0xCC797979);
+
+  static const groupEditColor = Color(0xFFC3C3C3);
+
+  static const error = Color(0xFFFF4D4F);
+
+  static final shadow = GUColors.black.withOpacity(0.1);
+
+  static const gray = Color(0xFFD9D9D9);
+}
